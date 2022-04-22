adf841097cd2e907ef5cf3879d29039fcc05ec2b
Lucas <lufbasilio@hotmail.com>
2022-04-22 12:09:43 -0300

    Finalizando modulo 5

---
+++ Udemy/mod5/cores.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo -e "\033[32;1;4mLUCAS"
+
+#Código Alteração
+#1 Negrito
+#2 Borrado
+#4 Sublinhado
+#5 Piscante
+#7 Reverso
+#8 Escondido
+#30 Preto
+#31 Vermelho
+#32 Verde
+#33 Amarelo
+#34 Azul
+#35 Roxo
+#36 Ciano
+#37 Cinza Claro
+#40 Fundo Preto
+#41 Fundo Vermelho
+#42 Fundo Verde
+#43 Fundo Amarelo
+#44 Fundo Azul
+#45 Fundo Roxo
+#46 Fundo Ciano
+#47 Fundo Cinza Claro
